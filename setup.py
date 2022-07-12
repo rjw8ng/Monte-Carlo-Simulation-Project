1d27f79d61587eceaa610c4fdd38bcb74e6ce568
rjw8ng <107445344+rjw8ng@users.noreply.github.com>
2022-07-11 23:33:01 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Die_Game',
+      version='0.1',
+      description='Monte Carlo Simulation',
+      url='https://github.com/rjw8ng/DS-5100-Final-Project',
+      author='Royal Wang',
+      author_email='rjw8ng@virginia.edu',
+      license='MIT',
+      packages=['Die'])
